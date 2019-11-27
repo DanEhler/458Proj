18b8301cff9fc2ed852fa0c564e9b1905068e59d
DanEhler <thurgoodmagee@gmail.com>
2019-11-27 16:58:53 -0600

    Finished schedule tests

--- RMS.py
+++ RMS.py
@@ -1,16 +1,30 @@
+import math
+
+# Will be from task parser
 set1 = {
-    "c": 5,
-    "p": 10
+    "c": 1,
+    "p": 8
 }
 set2 = {
-    "c": 6,
-    "p": 10
+    "c": 2,
+    "p": 6
 }
-task_set = [set1, set2]
+set3 = {
+    "c": 4,
+    "p": 24
+}
+task_set = [set1, set2, set3]
 
 
 def rms(task_set):
-    print(rms_utilization(set))
+    if rms_utilization(task_set) is True:
+        print("Schedulable under utilization test")
+    else:
+        print("Not schedulable under utilization test")
+    if exact_analysis(task_set) is True:
+        print ("Schedulable under exact analysis")
+    else:
+        print("Not schedulable under exact analysis")
 
 
 def rms_utilization(task_set):
@@ -26,7 +40,35 @@ def rms_utilization(task_set):
 
 
 def exact_analysis(task_set):
-    return
+    task_set = sorted(task_set, key=lambda i: i['p'], reverse=True)  # sort by period
+    for task in task_set:
+        dt = task['p']
+        to = sum(task['c'] for task in task_set)
+        if inner_exact(to, task_set, dt) is False:
+            return False
+        else:
+            task_set.pop(0)
+    # Work around for last entry
+    dt = task['p']
+    to = sum(task['c'] for task in task_set)
+    if inner_exact(to, task_set, dt) is False:
+        return False
+    else:
+        return True
+
+
+# recursive method to get exact analysis
+def inner_exact(t, task_set, dt):
+    total = 0
+    for task in task_set:
+        total += task['c'] * math.ceil(t / task['p'])
+    if total is t:
+        if t <= dt:
+            return True
+        else:
+            return False
+    else:
+        inner_exact(total, task_set, dt)
 
 
 if __name__ == "__main__":
